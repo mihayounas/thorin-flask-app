444bc1e1f68822333b8c85f9ec8d616f370da4d3
mihayounas <mihaelayounas55@gmail.com>
2022-07-23 10:42:47 +0000

    Add basic html

---
+++ run.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h1>Hello,<h2>World</h2></h1>"
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
