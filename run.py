e6b8ae26e7a9b56ce9d4bc66cacd464e8ca31d99
mihayounas <mihaelayounas55@gmail.com>
2022-07-27 22:28:18 +0000

    Add template bootstrap

--- run.py
+++ run.py
@@ -1,12 +1,28 @@
 import os
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "<h1>Hello,<h2>World</h2></h1>"
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
 
 
 if __name__ == "__main__":
